23cca69bd5c2d4dde18cbd5c83a438524379d0df
kaiser-majestad <nelsonalzateg@donbosco.edu.co>
2025-04-09 16:57:37 -0500

    thrir

--- static/js/agregargenero.js
+++ static/js/agregargenero.js
@@ -16,35 +16,31 @@ document.getElementById("formGenero").addEventListener("submit", function(event)
     .then(data => {
         const parser = new DOMParser();
         const doc = parser.parseFromString(data, "text/html");
-
-const estado = doc.querySelector('#alert-container').getAttribute('data-estado');
-const mensaje = doc.querySelector('#alert-container').getAttribute('data-mensaje');
-if (estado === "True") {
-    Swal.fire({
-    icon: 'success',
-    title: '¡Éxito!',
-    text: mensaje,
-    confirmButtonText: 'Aceptar'
-}).then(() => {
-    window.location.href = '/listgenero/';
-}
-);
-} else 
-{
-Swal.fire({
-    icon: 'error',
-    title: '¡Error!',
-    text: mensaje,
-    confirmButtonText: 'Aceptar'
-});
-}
-}).catch(error => {
-    Swal.fire("Error en la solicitud",error);
-    Swal.fire({
-    icon: 'error',
-    title: '¡Algo salió mal!',
-    text: "Hubo un problema al procesar la solicitud.",
-    confirmButtonText: 'Aceptar'
+        const estado = doc.querySelector('#alert-container').getAttribute('data-estado');
+        const mensaje = doc.querySelector('#alert-container').getAttribute('data-mensaje');
+        if (estado === "True") {
+            Swal.fire({
+                icon: 'success',
+                title: '¡Éxito!',
+                text: mensaje,
+                confirmButtonText: 'Aceptar'
+            }).then(() => {
+            window.location.href = '/listgenero/';
+        });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: '¡Error!',
+            text: mensaje,
+            confirmButtonText: 'Aceptar'});
+        }}
+    ).catch(error => {
+        Swal.fire("Error en la solicitud",error);
+        Swal.fire({
+            icon: 'error',
+            title: '¡Algo salió mal!',
+            text: "Hubo un problema al procesar la solicitud.",
+            confirmButtonText: 'Aceptar'
         });
     });
 });
